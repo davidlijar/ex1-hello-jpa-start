90d8288b857974df90c4ee1404647c412303d26e
Li Jar <davidbeljar@gmail.com>
2024-07-05 18:19:26 +0900

    comt

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,9 +8,35 @@ public class JpaMain {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
+        EntityTransaction tx = em.getTransaction();
 
-        ///changed
+        tx.begin();
+
+        try{
+            Team team = new Team();
+            team.setName("A");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("Lijar");
+            member.setTeam(team);
+            em.persist(member);
+
+            //with this the below(em.find)  is from DB query
+            //without this the below(em.find)  is from 1차 캐시 query
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+            System.out.println("findMember = " + findMember.getTeam().getName());
+
+            tx.commit();
+        }catch(Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
 
         em.close();
         emf.close();
